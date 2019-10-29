854d77c68ce78f154e136cc31409c4593b811609
Farhan <fmshyder@gmail.com>
2019-10-28 21:31:28 -0400

    init setup

---
+++ Tutorials/chitty-hyder/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import Dashboard from './Dashboard'
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>hellowwww</h1>
+    
+    
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
