c74744ef1fd020915fb29d8b6a9cc1500d721ad1
Shi B <sh2581@nyu.edu>
2019-12-10 10:20:15 -0500

    Added delete chatroom UI and functionality

---
+++ src/app/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Inject, Optional } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogData } from 'src/app/models/confirmation-dialog-data';
+import { ChatroomService } from '../services/chatroom.service';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrls: ['./confirmation-dialog.component.scss']
+})
+export class ConfirmationDialogComponent implements OnInit {
+
+  title: string;
+  prompt: string;
+
+  constructor(
+    public chatroomService: ChatroomService,
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  ngOnInit() {
+    this.prompt = this.data.prompt;
+    this.title = this.data.title;
+  }
+
+  deleteChatroom(): Promise<any> {
+    return this.chatroomService
+      .deleteChatroom(this.data.chatroomID)
+      .then(() => {
+        this.data.callback();
+        this.closeDialog();
+      });
+  }
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+
+}
