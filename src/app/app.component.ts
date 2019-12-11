c03712eb12c41401c21b4607900b4da504d3492f
Angelica Hernandez <angelica98h@gmail.com>
2019-12-11 18:44:13 -0500

    finally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from './services/auth.service';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { UserInfoService } from './services/user-info.service';
 import { ChatroomService } from './services/chatroom.service';
+import { Observable } from 'rxjs';
 
 /**
  * App component is the base page for logging in
@@ -17,7 +18,7 @@ export class AppComponent implements OnInit {
    * constructor of base app component
    * @param auth instance of AuthService declared as public
    */
-
+  isDarkTheme: Observable<boolean>;
   constructor(public auth: AuthService,
               private chatroomService: ChatroomService,
               private angularFireAuth: AngularFireAuth,
